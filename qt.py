762a1c1c631af4e930c58b6ca96004b36dd1dd5f
--global <nate@keemail.me>
2016-04-19 17:28:40 -0500

    Work on UI

--- qt.py
+++ qt.py
@@ -1,45 +1,19 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
 import sys
-from PyQt5.QtWidgets import (QWidget, QToolTip, 
-    QPushButton, QApplication, QListView)
-from PyQt5.QtGui import QFont   
-from PyQt5.QtGui import QStandardItemModel, QStandardItem
+
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow
+from PyQt5.uic import loadUi
+
+
+class DemoImpl(QMainWindow):
+    def __init__(self, *args):
+        super(DemoImpl, self).__init__(*args)
+
+        loadUi('ui/TiKi Music.ui', self)
 
 
 
-class Example(QWidget):
-    
-    def __init__(self):
-        super().__init__()
-        
-        self.initUI()
-        
-        
-        
-    def initUI(self):
-        
-        # Basic button
-        btn = QPushButton('Button', self)
-        btn.move(0, 0)       
-        btn.clicked.connect(self.blah)
-
-        
-        
-        self.setGeometry(300, 300, 300, 200)
-        self.setWindowTitle('TiKi Music Player')    
-        self.show()
-        
-        
-        
-    def blah(self):
-        print ('BLAHHHHH')
-        
-        
-        
-if __name__ == '__main__':
-    
-    app = QApplication(sys.argv)
-    ex = Example()
-    sys.exit(app.exec_())
+app = QApplication(sys.argv)
+widget = DemoImpl()
+widget.show()
+sys.exit(app.exec_())

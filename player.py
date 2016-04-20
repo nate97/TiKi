028d4f889a38107a1572bd953c999c0ec29ecd93
--global <nate@keemail.me>
2016-04-19 23:55:05 -0500

    UI changes

--- player.py
+++ player.py
@@ -1,26 +1,50 @@
 import vlc
 import time
 
+
 class TiKiPlayer:
     def __init__(self):
         print ('Started playing audio')
         # Audio file properties
         self.filename = ''
         self.audiolength = 0
+        # Initialize self.player
+        self.player = None
         
-
-    def playAudio(self, filename, audiolength):
+        
+    # Set the name of the song file
+    def setFileName(self, filename):
+        self.filename = filename
+        
+    
+    # Define the ending point of the song
+    def setAudioLength(self, audiolength):
+        self.audiolength = audiolength
+        
+        
+    # Start the media file
+    def playAudio(self):
         instance = vlc.Instance()
 
         #Create a MediaPlayer with the default instance
-        player = instance.media_player_new()
+        self.player = instance.media_player_new()
 
         #Load the media file
-        media = instance.media_new(filename)
+        media = instance.media_new(self.filename)
 
         #Add the media to the player
-        player.set_media(media)
+        self.player.set_media(media)
 
         # Play for the length of the song then exit
-        player.play()
-        time.sleep(audiolength)
+        self.player.play()
+        time.sleep(self.audiolength)
+        
+        
+    def setVolume(self, volume):
+        pass
+        
+        
+    # Stop currently playing audio
+    def stopAudio(self):
+        pass
+        self.player.stop()
